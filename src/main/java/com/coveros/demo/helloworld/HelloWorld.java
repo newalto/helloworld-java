cffd4a9d389de33a548ee954a50a34e67b0d2ead
liupeng <tux-liupeng@qq.com>
2021-12-24 17:45:29 +0800

    demo print os name, arch, version

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -10,6 +10,18 @@ public class HelloWorld {
     final LocalDateTime now = LocalDateTime.now();
 
     System.out.println("Hello, World! The current time is " + dtf.format(now));
+
+    //当前系统名称
+    String OS_NAME = System.getProperty("os.name").toLowerCase();
+    System.out.println(OS_NAME);
+
+    //当前系统架构
+    String OS_ARCH = System.getProperty("os.arch").toLowerCase();
+    System.out.println(OS_ARCH);
+
+    //当前系统内核版本
+    String OS_VERSION = System.getProperty("os.version").toLowerCase();
+    System.out.println(OS_VERSION);
   }
 
 }
